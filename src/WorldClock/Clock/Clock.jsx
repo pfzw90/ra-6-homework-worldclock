a6bd078837bed702d28afd707539a3aa8eb22927
Natalya Chekhonina <pfzw90@gmail.com>
2021-08-16 10:58:44 +0300

    initial

---
+++ src/WorldClock/Clock/Clock.jsx
@@ -0,0 +1,57 @@
+import moment from 'moment'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+export default class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.city = props.city
+        this.offset = parseInt(props.offset)
+        this.id = props.id
+        this.currentTime = moment.utc().utcOffset(this.offset).toObject() 
+        this.state = {
+            hours: this.currentTime.hours,
+            minutes: this.currentTime.minutes,
+            seconds: this.currentTime.seconds 
+        }     
+    }
+
+    
+
+    componentDidMount () {      
+        this.hoursTimer = setInterval(()=>this.setState((prev)=> ({...prev, hours: prev.hours + 1})), 1000 * 60 * 60);
+        this.minutesTimer = setInterval(()=>this.setState((prev)=> ({...prev, minutes: prev.minutes + 1})), 1000 * 60);
+        this.secondsTimer = setInterval(()=>this.setState((prev)=> ({...prev, seconds: prev.seconds + 1})), 1000);
+    }
+
+    handleDelete = () => {
+        const container = document.getElementById(this.id);
+        ReactDOM.unmountComponentAtNode(container)
+        container.remove()
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.hoursTimer);
+        clearInterval(this.minutesTimer);
+        clearInterval(this.secondsTimer);
+    }
+
+    render() {
+        return(
+        <div className="ClockContainer">
+            <div className="ClockInfo">
+                <span className="City">{this.city}</span><span className="ClockRemove" onClick={this.handleDelete}>✘</span>
+            </div>
+            <div className="Clock">
+                <div className="wrap">
+                    <span className="hour" style={{transform :`rotate(${this.state.hours*30}deg)`}}></span>
+                    <span className="minute"style={{transform :`rotate(${this.state.minutes*6}deg)`}}></span>
+                    <span className="second"style={{transform :`rotate(${this.state.seconds*6}deg)`}}></span>
+                    <span className="dot"></span>
+                </div>
+            </div>
+        </div>    
+        )
+    }
+}
---
+++ src/WorldClock/Worldclock.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as data from './../json/timezones.json'
+import shortid from 'shortid'
+import Clock from './Clock/Clock'
+import './WorldClock.css'
+
+    const TIMEZONES = data.default
+    console.log(TIMEZONES[0])
+  
+    
+
+export default function WorldClock() {
+
+    const handleClockFormSubmit = (e) => {    
+        e.preventDefault();
+        const id = shortid.generate();
+        const data = Array.from(e.target.timezoneselect.options).find(o=>o.selected).dataset
+        const clock = <Clock offset={data.offset} city={data.city} id={id}/>
+        const container = document.createElement('div');
+        container.id = id;
+        document.getElementById("worldclock").appendChild(container)
+        ReactDOM.render(clock, container);
+        e.target.reset()
+    }
+      
+    
+    return (
+        <div className="WorldClock" id="worldclock">
+            <form className="ClockForm" onSubmit={handleClockFormSubmit}>
+                <select className="TimezoneSelect" id="timezoneselect" name="timezoneselect">
+                    {TIMEZONES.map(tz=> (
+                        tz.utc.map(utc=> 
+                            {const city = String(utc.split('/')[1]).replace('_',' ')
+                            return (<option key={shortid.generate()} data-offset={tz.offset} data-city={city}>
+                                {
+                                `${city} 
+                                [UTC ${tz.offset > 0 ? '+': ''}${tz.offset}]`
+                                }
+                                </option>)
+                            })
+                    ))}
+                </select>
+                <button className="ClockForm-button" type="submit">Добавить</button>
+            </form>
+        </div>
+    )
+
+
+}
